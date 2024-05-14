d9b73e567051c40608f1997271b21b6f915a2aa3
Randolfo A Goncalves <ragnifico@yahoo.com.br>
2024-05-14 16:59:10 -0300

    test simples

---
+++ src/test/ProductTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import dados.Product;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @Test
+    void set_name() {
+    }
+
+    @Test
+    void set_code() {
+    }
+}
---
+++ src/test/UserTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import dados.User;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @org.junit.jupiter.api.Test
+    void set_uname() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void is_phone() {
+        Assertions.assertTrue(User.is_phone("(31)98105-9465"),"Número correto.");
+        Assertions.assertFalse(User.is_phone("(31)8105-9465"),"Número incompleto.");
+        Assertions.assertFalse(User.is_phone("(3A)A105-9465"),"Número errado.");
+    }
+
+    @org.junit.jupiter.api.Test
+    void is_email() {
+        Assertions.assertTrue(User.is_email("ragnifico@yahoo.com.br"),"Email pessoal.");
+        Assertions.assertTrue(User.is_email("mabtoy@gmail.com"),"email profissional");
+        Assertions.assertFalse(User.is_email("ragnifico@@yahoo.com.br"),"Erro @@.");
+        Assertions.assertFalse(User.is_email("ragnifico@yahoo."),"Erro de email incompleto.");
+    }
+}
