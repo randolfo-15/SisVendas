1ce6f615ca7c38610dbfb212a1b96a19038c2d3f
Pablo Rangel Abreu Andrade <pabloandrade@houer.com.br>
2024-05-15 20:26:34 -0300

    Tela login - Pablo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {  }
+    public static void main(String[] args) {
+
+        new LoginForm(null);
+
+    }
 }
