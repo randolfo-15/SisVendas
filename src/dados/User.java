4e2c5a741e797805b5613670b209b524438412f5
Randolfo <ragnifico@yahoo.com.br>
2024-05-15 19:41:15 -0300

    Atribuição a classe User dos campos:
     passw(Password ou senha):
     |
     |----> void set_passw(String);
     |----> String get_passw();
    
     adm(Ser ou não ser administrador):
     |
     |----> void set_adm(boolean);
     |----> boolean get_adm();
    
     Para realizar mudanças no campo passw, faça um try catch com a exception Sulficient data.
    
     Randolfo A Goncalves - 15/05/24

--- src/dados/User.java
+++ src/dados/User.java
@@ -8,7 +8,10 @@ public class User {
             name  = "",
             uname = "",
             phone = "",
-            email = "";
+            email = "",
+            passw = "";
+
+    boolean adm = false;
 
     private static final String
             regex_phone = "\\(\\d{2}\\)\\d{5}-\\d{4}",
@@ -26,6 +29,7 @@ public class User {
     public static class Existing_phone extends Exception{ public String msg(){ return "Este número já se encontra em uso."; }}
     public static class Existing_email extends Exception{ public String msg(){ return "Este email já se encontra em uso.";  }}
     public static class Invalid_format extends Exception{ public String msg(){ return "Formato de dado invalido";           }}
+    public static class Sulficient_data extends Exception{ public String msg(){ return "Senha com menos de 6 dígitos.";     }}
 
     //==================================================================================================================
     // Getting
@@ -34,11 +38,19 @@ public class User {
     public String get_uname(){ return uname; }
     public String get_phone(){ return phone; }
     public String get_email(){ return email; }
-
+    public String get_passw(){ return passw; }
+    public boolean get_adm() { return adm;   }
     //==================================================================================================================
     // Setting
     //==================================================================================================================
     public void set_name(String name) { this.name=name.toUpperCase(); }
+
+    public void set_adm(boolean adm){ this.adm=adm; }
+
+    public void set_passw(String passw) throws Sulficient_data {
+        if(passw.length()<6) throw new Sulficient_data();
+        this.passw = passw;
+    }
 
     public void set_uname(String uname) throws Existing_name {
         if(/*Query.exist(SQL.TABLE_USER,SQL.COLUMN_UNAME,uname)*/false ) throw new Existing_name();
