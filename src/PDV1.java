9dd1b0de4fcba20781720431625a5c5cc2c9e7a8
Pablo Rangel Abreu Andrade <pabloandrade@houer.com.br>
2024-05-28 19:36:21 -0300

    Nota Tela PDV

---
+++ src/PDV1.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class PDV1 {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JRadioButton dinheiroRadioButton;
+    private JRadioButton cartãoDeCréditoRadioButton;
+    private JRadioButton cartãoDeDébitoRadioButton;
+    private JRadioButton PIXRadioButton;
+    private JRadioButton cartãoAlimentaçãoRadioButton;
+    private JRadioButton cartãoRefeiçãoRadioButton;
+    private JButton finalizarCompraButton;
+
+
+    public static void totalCompra(double valor){
+
+
+
+
+    }
+}
