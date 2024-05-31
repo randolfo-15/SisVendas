b5c14ba4090ef50f32816f829d40843985d08ba3
Randolfo A Goncalves <ragnifico@yahoo.com.br>
2024-05-31 15:28:28 -0300

    New apt

---
+++ src/LoginForm.java
@@ -0,0 +1,118 @@
+import dados.User;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class LoginForm extends JDialog {
+    private JPanel loginPanel;
+    private JTextField tfEmail;
+    private JPasswordField pfPassword;
+    private JButton btnOK;
+    private JButton btnCancel;
+
+    public LoginForm(JFrame parent){
+        super(parent);
+        setTitle("login");
+        setContentPane(loginPanel);
+        setMinimumSize(new Dimension(450,474));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        //setVisible(true);
+
+        btnOK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String email = tfEmail.getText();
+                String password = String.valueOf(pfPassword.getPassword());
+
+                user = getAuthenticatedUser(email, password);
+
+                if(user != null){
+                    dispose();
+                }
+                else{
+                    JOptionPane.showMessageDialog(LoginForm.this,
+                            "Email or password invalid",
+                            "Try again",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                dispose();
+
+
+            }
+        });
+
+        setVisible(true);
+    }
+
+    private User user;
+
+    private User getAuthenticatedUser(String email, String password){
+
+        User user = null;
+        final String DB_URL = "jdbc:mysql://localhost/mystore?serverTimezone=UTC";
+        final String USERNAME = "root";
+        final String PASSWORD = "";
+
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME,PASSWORD);
+
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM users WHERE email=? AND password=?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()){
+                user = new User();
+                user.name = resultSet.getString("name");
+                user.email = resultSet.getString("email");
+                user.phone = resultSet.getString("phone");
+                user.passw = resultSet.getString("password");
+            }
+
+            stmt.close();
+            conn.close();
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+
+        }
+
+        return user;
+    }
+
+    public static void main(String[] args) {
+        LoginForm loginForm = new LoginForm(null);
+        User user = loginForm.user;
+
+        if(user!= null){
+            System.out.println("Sucessful Authentication: " + user.name);
+            System.out.println("Email: " + user.email);
+            System.out.println("Phone: " + user.phone);
+
+        }
+        else{
+
+            System.out.println("Authentication canceled");
+        }
+    }
+
+    public User usuarioAtual(){
+
+        return user;
+    }
+}
